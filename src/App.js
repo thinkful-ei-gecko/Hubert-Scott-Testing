21c55334bd5db26dddf7f30774a1808864496531
Hubert Yang <offspring7890@yahoo.com>
2019-09-03 16:34:15 -0700

    working app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React from 'react';
-import STORE from './STORE'
 import './index.css';
 import List from './List'
+import './style.css';
 
 function App(props) {
   const lists = props.store.lists;
   const allCards = props.store.allCards;
   const cardsOne = lists[0].cardIds.map(id => allCards[id]);
-  //const cardsOne = list[id].cardIds.map(id => allCards[id]);
-  // const cardsObj = cards.filter(id => {})
-  // const itemsArr = items.map(item => {
-  //  return 
-  // })
   return (
     <main className='App'>
       <header className="App-header">
         <h1>Trelloyes!</h1>
     </header>
       <div className="App-list">
-        {lists.map(list => (
+        {lists.map((list, i) => (
             <List 
               key = {list.id} 
               header = {list.header}
-              cards = {cardsOne} />
+              cards = {lists[i].cardIds.map(id => allCards[id])} />
         ))}
       </div>
     </main>
--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,6 @@
 import './index.css'
 import React from 'react';
-import ReactDOM from 'react-dom';
+import './style.css';
 
 function Card(props) {
     return (
