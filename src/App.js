61ed9d5ebd31cd4ab75c2fcfdd186d57fadb7ce9
Hubert Yang <offspring7890@yahoo.com>
2019-09-03 15:14:22 -0700

    edit app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import List from './List'
 function App(props) {
   const lists = props.store.lists;
   const allCards = props.store.allCards;
-  const cardsOne = lists[id].cardIds.map(id => allCards[id]);
+  const cardsOne = lists[0].cardIds.map(id => allCards[id]);
   //const cardsOne = list[id].cardIds.map(id => allCards[id]);
   // const cardsObj = cards.filter(id => {})
   // const itemsArr = items.map(item => {
@@ -18,13 +18,12 @@ function App(props) {
         <h1>Trelloyes!</h1>
     </header>
       <div className="App-list">
-        {lists.map(list => {
+        {lists.map(list => (
             <List 
               key = {list.id} 
               header = {list.header}
               cards = {cardsOne} />
-              }
-        )}
+        ))}
       </div>
     </main>
   );
