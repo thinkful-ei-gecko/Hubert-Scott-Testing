17c7d9240bbd9806990f41009f67293da067efc8
Hubert Yang <offspring7890@yahoo.com>
2019-09-03 11:38:22 -0700

    create card.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import STORE from './STORE'
+import './index.css'
+
+function App(STORE) {
+  return (
+    <main className='App'>
+      {/* content goes here */}
+    </main>
+  );
+}
+
+export default App;
