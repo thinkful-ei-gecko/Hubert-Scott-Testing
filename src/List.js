751e2dea135146c21f76080d47fc5f9b0477cccd
preet-singh <preetsingh13@gmail.com>
2019-09-03 14:31:12 -0500

    list.js update

--- src/List.js
+++ src/List.js
@@ -1,1 +1,27 @@
-import './index.css'
+import React from 'react';
+import Card from './Card';
+import './index.css';
+
+function List(props){
+    return (
+        <section className="List">
+            <header className="List-header">
+                <h2>{props.header}</h2>
+            </header>
+         <div className="List-cards">
+            {props.Card.map((card) =>
+               <Card 
+                    key = {card.id}
+                    title = {card.title}
+                    content = {card.content} />
+            )}
+            <button type="button" className="List-button">
+                delete
+            </button>
+         </ div>
+        </ section>     
+    )
+}
+
+export default App;
+
