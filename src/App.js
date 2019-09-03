15a392d158c74a96d3fd237ec6b632e7470baf29
Hubert Yang <offspring7890@yahoo.com>
2019-09-03 14:58:11 -0700

    edit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import List from './List'
 function App(props) {
   const lists = props.store.lists;
   const allCards = props.store.allCards;
-  const cardsOne = lists.cardIds.map(id => allCards[id]);
+  const cardsOne = lists[0].cardIds.map(id => allCards[id]);
   // const cardsObj = cards.filter(id => {})
   // const itemsArr = items.map(item => {
   //  return 
